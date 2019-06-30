a5b0340870d444e6816a83a0dcf818298dd32b91
Marcelo Tavares <marcelo_tavares@outlook.com>
2019-06-29 21:58:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import requests
-import zipfile
-import os
+from glob import glob
+from os import path, remove, getcwd
+from platform import system
+from time import sleep
+from zipfile import ZipFile
+
+from requests import get
 
 
 def download_file(url):
     local_filename = url.split('/')[-1]
-    r = requests.get(url, stream=True)
+    r = get(url, stream=True)
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
@@ -15,16 +19,24 @@ def download_file(url):
 
 
 def unzip(filezip):
-    folder = os.getcwd()
-    with zipfile.ZipFile(filezip, "r") as zip_ref:
-        zip_ref.extractall(folder)
+    with ZipFile(filezip, 'r') as zip_ref:
+        zip_ref.extractall(getcwd())
 
-    os.remove(r"{}\{}".format(folder, filezip))
+    remove(path.join(getcwd(), filezip))
 
 
-def chromedriver():
-    if not os.path.exists("chromedriver.exe"):
-        lr = requests.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
+def check_driver():
+    systems = {'Darwin': 'mac64', 'Linux': 'linux64', 'Windows': 'win32'}
+    os = systems[system()]
+    base_url = 'https://chromedriver.storage.googleapis.com'
+
+    if not glob('chromedriver*'):
+        lr = get('{url}/LATEST_RELEASE'.format(url=base_url))
         last_version = lr.text
-        download_file("https://chromedriver.storage.googleapis.com/{}/chromedriver_win32.zip".format(last_version))
-        unzip("chromedriver_win32.zip")
+        download_file('{url}/{lv}/chromedriver_{os}.zip'.format(url=base_url, lv=last_version, os=os))
+        sleep(5)
+        unzip('chromedriver_{}.zip'.format(os))
+
+
+if __name__ == '__main__':
+    check_driver()
